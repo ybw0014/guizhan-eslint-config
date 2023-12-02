2f35d475cb30b5d7a1380676a481c2152c13ce27
ybw0014 <i@guizhanss.com>
2023-12-02 11:21:57 -0500

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# ESlint Config
+
+`@guizhan/eslint-config`
+
+## Install
+
+```bash
+npm install @guizhan/eslint-config --save-dev
+```
+
+## Usage
+
+```js
+// .eslintrc.js
+{
+  'extends': [
+    '@guizhan/eslint-config'
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'import/no-unresolved': ['off'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
+    'eol-last': ['error', 'always'],
+    'max-len': ['error', { code: 120, tabWidth: 2 }]
+  }
+};
